c6a28cf81969bb1eb9d8053591816ed68ae38a3d
Meepmeme <you.example.com>
2022-05-18 16:47:30 -0400

    First working version, meets all pt.1 req's

--- Main.java
+++ Main.java
@@ -1,78 +1,129 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.table.*;
 
 public class Main extends JFrame {
+  // private vars here
+  private JTable tbl1;
+  private JLabel gameInfo;
+  private JLabel label1;
+  private JTextField tf1;
+  private JButton btn1;
+  private JButton btn2;
+  private DefaultTableModel model;
+
+  public Main(Minesweeper s) {
+    super("JMinesweeper");
+    this.setSize(500, 500);
+    this.getContentPane().setBackground(Color.white);
+    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    this.setLayout(new GridBagLayout());
+    GridBagConstraints c = new GridBagConstraints();
+
+    // outer rows and cols will have coordinate guides in form |n|
+    // allows me to have full minesweeper name but use single-digit coords
+    model = new DefaultTableModel(s.vals, s.colTitles());
+    tbl1 = new JTable(model);
+    c.gridx = 0;
+    c.gridy = 0;
+    c.gridwidth = 5;
+    c.gridheight = 5;
+    this.add(tbl1, c);
+
+    gameInfo = new JLabel(
+        "Minesweeper clone. \"_\" is clear, \"f\" is flagged, and any number is how many bombs are near.");
+    c.gridx = 0;
+    c.gridy = 5;
+    c.gridwidth = 5;
+    c.gridheight = 1;
+    this.add(gameInfo, c);
+
+    label1 = new JLabel("[X,Y]: [?,?]");
+    c.gridx = 0;
+    c.gridy = 6;
+    c.gridwidth = 2;
+    c.gridheight = 1;
+    this.add(label1, c);
+
+    tf1 = new JTextField(3); // "X,Y". will be read as XCoord, *, YCoord
+    c.gridx = 2;
+    c.gridy = 6;
+    c.gridwidth = 1;
+    c.gridheight = 1;
+    this.add(tf1, c);
+
+    btn1 = new JButton("Select");
+    c.gridx = 3;
+    c.gridy = 6;
+    c.gridwidth = 1;
+    c.gridheight = 1;
+    this.add(btn1, c);
+
+    btn2 = new JButton("Flag");
+    c.gridx = 4;
+    c.gridy = 6;
+    c.gridwidth = 1;
+    c.gridheight = 1;
+    this.add(btn2, c);
+
+    Handler hand = new Handler(s, model);
+    // add things to handler here
+    tf1.addActionListener(hand);
+    btn1.addActionListener(hand);
+    btn2.addActionListener(hand);
+
+    this.pack();
+    this.setVisible(true);
+  }
+
+  private class Handler implements ActionListener {
     // private vars here
-    private JTable tbl1;
-    private JLabel gameInfo;
-    private JLabel label1;
-    private JTextField tf1;
-    private JButton btn1;
-    private JButton btn2;
-    private int[][] bombs;;
+    private int clickX;
+    private int clickY;
+    private String tmp;
+    private Minesweeper s;
+    private DefaultTableModel f;
 
-    public Main() {
-        super("JMinesweeper");
-        bombs = new int[2][11]; // 2 coords per bomb, 11 bombs
-        this.setSize(500, 500);
-        this.getContentPane().setBackground(Color.white);
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-
-        String[][] vals = new String[11][11];
-        // outer rows and cols will have coordinate guides in form |n|
-        // allows me to have full minesweeper name but use single-digit coords
-        String[] colTitles = { "M", "I", "N", "E", "S", "W", "E", "E", "P", "E", "R" };
-        tbl1 = new JTable(vals, colTitles);
-        c.gridx = 0;
-        c.gridy = 0;
-        c.gridwidth = 5;
-        c.gridheight = 5;
-        this.add(tbl1, c);
-
-        gameInfo = new JLabel("Total bombs: 0, Bombs flagged: 0");
-        c.gridx = 0;
-        c.gridy = 5;
-        c.gridwidth = 5;
-        c.gridheight = 1;
-        this.add(gameInfo, c);
-
-        label1 = new JLabel("[X,Y]: [?,?]");
-        c.gridx = 0;
-        c.gridy = 6;
-        c.gridwidth = 2;
-        c.gridheight = 1;
-        this.add(label1, c);
-
-        tf1 = new JTextField(3); // "X,Y". will be read as XCoord, *, YCoord
-        c.gridx = 2;
-        c.gridy = 6;
-        c.gridwidth = 1;
-        c.gridheight = 1;
-        this.add(tf1, c);
-
-        btn1 = new JButton("Select");
-        btn2 = new JButton("Flag");
-
-        Handler hand = new Handler();
-        // add things to handler here
-        tf1.addActionListener(hand);
-
-        this.pack();
-        this.setVisible(true);
+    public Handler(Minesweeper seep, DefaultTableModel tm) {
+      super();
+      s = seep;
+      f = tm;
     }
 
-    private class Handler implements ActionListener {
-        // private vars here
-
-        public void actionPerformed(ActionEvent event) {
-            // do something
-        }
+    public void actionPerformed(ActionEvent event) {
+      // do something
+      if (event.getSource() == tf1) {
+        tmp = event.getActionCommand();
+        clickX = getInt(tmp.substring(0, 1));
+        clickY = getInt(tmp.substring(2, 3));
+        label1.setText("[X,Y]: [" + clickX + "," + clickY + "]");
+        JOptionPane.showMessageDialog(null, "set cursor coords to " + clickX +
+                                                "," + clickY);
+      }
+      if (event.getSource() == btn1) {
+        JOptionPane.showMessageDialog(null, s.clickSpot(clickX, clickY, false));
+        f.setDataVector(s.vals, s.colTitles());
+      }
+      if (event.getSource() == btn2) {
+        JOptionPane.showMessageDialog(null, s.clickSpot(clickX, clickY, true));
+        f.setDataVector(s.vals, s.colTitles());
+      }
     }
+  }
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(Main::new);
+  public static void main(String[] args) {
+    Minesweeper sweep = new Minesweeper();
+    new Main(sweep);
+  }
+
+  public int getInt(String str) {
+    int number = 0;
+    try {
+      number = Integer.parseInt(str);
+    } catch (NumberFormatException ex) {
+      ex.printStackTrace();
     }
+    return number;
+  }
 }
---
+++ Minesweeper.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+
+public class Minesweeper {
+
+  private int[][] bombs;
+  public String[][] vals;
+  private ArrayList<String> checked;
+  private int bombCount;
+
+  public Minesweeper() {
+    bombCount = 21;
+    bombs = new int[2][bombCount]; // 2 coords per bomb, 20 bombs
+    checked = new ArrayList<String>();
+    generateBombs();
+    vals = new String[11][11];
+    for (int i = 1; i < 10; i++) {
+      vals[0][i] = "\\" + i + "/";
+      vals[10][i] = "/" + i + "\\";
+      vals[i][0] = "|" + i + "|";
+      vals[i][10] = "|" + i + "|";
+    }
+  }
+
+  public int getBombNum() {
+    return bombCount;
+  }
+
+  public String[] colTitles() {
+    return new String[] { "M", "I", "N", "E", "S", "W", "E", "E", "P", "E", "R" };
+  }
+
+  public void generateBombs() {
+    for (int i = 1; i < bombs[0].length - 1; i++) {
+      bombs[0][i] = ((int) (Math.random() * 9)) + 1;
+      bombs[1][i] = ((int) (Math.random() * 9)) + 1;
+    }
+  }
+
+  private void updateGameGrid(int x, int y) {
+    boolean isChecked = false;
+    if (!(x == 0 || x == 10 || y == 0 || y == 10)) {
+      if (!(isBomb(x, y))) {
+        if (getBombCount(x, y) != 0) {
+          vals[x][y] = (getBombCount(x, y) + "");
+        } else {
+          for (int i = 0; i < checked.size(); i++) {
+            if (checked.get(i).equals((x + "," + y)))
+              isChecked = true;
+          }
+          if (isChecked == false) {
+            checked.add(x + "," + y);
+            vals[x][y] = "_";
+            updateGameGrid(x - 1, y);
+            updateGameGrid(x, y - 1);
+            updateGameGrid(x, y + 1);
+            updateGameGrid(x + 1, y);
+          }
+          checked.add(x + "," + y);
+        }
+      }
+    }
+  }
+
+  private int getBombCount(int x, int y) {
+    int bmb = 0;
+    for (int i = x - 1; i < x + 1; i++) {
+      for (int j = y - 1; j < y + 1; j++) {
+        if (isBomb(i, j))
+          bmb++;
+      }
+    }
+    return bmb;
+  }
+
+  private boolean isBomb(int x, int y) {
+    for (int i = 0; i < bombs[0].length; i++) {
+      if (bombs[0][i] == x && bombs[1][i] == y) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public String clickSpot(int x, int y, boolean flag) {
+    checked.clear();
+    if (isBomb(x, y)) {
+      if (flag) {
+        vals[x][y] = "f";
+        return ("Flagged " + x + "," + y);
+      } else {
+        vals[x][y] = "X";
+        return ("Clicked " + x + "," + y + ".\nIt was a bomb.\nRestart program for new game.");
+      }
+    } else {
+      if (flag) {
+        return ("Flagged " + x + "," + y);
+      } else {
+        updateGameGrid(x, y);
+        return ("Clicked " + x + "," + y + ".\nUpdated Game Grid.");
+      }
+    }
+  }
+}
