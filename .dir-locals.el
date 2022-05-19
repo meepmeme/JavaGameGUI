365e1f901884cf9cc2302fecf9caf870a399e3e8
Meepmeme <you.example.com>
2022-05-19 10:34:00 -0400

    add .dir-locals.el for porject-local emacs config

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((nil . (
+         (projectile-project-compilation-cmd . "javac -d ./out *.java")
+         (projectile-project-run-cmd . "javac -d ./out *.java && java -cp ./out Main")
+         )
+      ))
